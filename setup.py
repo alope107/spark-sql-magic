7ed2ea3bd8222ee05c4bb11e1716ee67f0088883
alope107 <auberonlopez@gmail.com>
2015-07-15 11:17:30 -0700

    Packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="sparksql",
+    version="0.0.1",
+    description="Spark SQL Magic",
+    author="Auberon López",
+    author_email="auberonlopez@gmail.com",
+    url="https://github.com/alope107/spark-sql-magic/",
+    packages=["sparksql"],
+    package_dir={"": "src"}
+)
